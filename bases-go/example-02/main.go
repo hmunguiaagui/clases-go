518f6b43fd44467079931500a6a966244ede062b
hmunguiaagui <hector.munguiaaguilera@mercadolibre.com.mx>
2023-12-28 10:58:07 -0600

    Example 2: declaring variables

---
+++ bases-go/example-02/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Weather variables example
+	var temperature int = 28
+	var wetness float64 = 77.7
+	var pressure float64 = 91.81
+
+	println(temperature)
+	println(wetness)
+	println(pressure)
+}
