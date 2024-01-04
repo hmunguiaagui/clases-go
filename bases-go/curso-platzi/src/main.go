c0a1bdaa30c60fcd9ddfcb83adbe8a8feebaef81
hmunguiaagui <hector.munguiaaguilera@mercadolibre.com.mx>
2024-01-04 10:35:17 -0600

    Platzi course

--- bases-go/curso-platzi/src/main.go
+++ bases-go/curso-platzi/src/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// Declare a constant
+	const pi float32 = 3.14
+	fmt.Println("pi", pi)
+	// Declare a variable
+	var base float64 = 10.1 // Explicit declaration
+	fmt.Println("base", base)
+	height := 8.2 // Implicit declaration
+	fmt.Println("height", height)
+	var area = base * height // Implicit declaration, default type is float64
+	fmt.Printf("area %f \n-----------\n", area)
+
+	// Arithmetic operators
+	numA := 10
+	numB := 5
+	fmt.Println("numA =", numA)
+	fmt.Println("numB =", numB)
+	fmt.Println("numA + numB =", numA+numB)
+	fmt.Println("numA - numB =", numA-numB)
+	fmt.Println("numA * numB =", numA*numB)
+	fmt.Println("numA / numB =", numA/numB)
+	fmt.Println("numA % numB =", numA%numB)
 }
