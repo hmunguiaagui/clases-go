264b59bf5be07f0ddcf43545adc1fa9e8f4413a5
hmunguiaagui <hector.munguiaaguilera@mercadolibre.com.mx>
2024-01-05 09:50:01 -0600

    Excercise 9: Read non existing file

--- bases-go/example-09/data-client/main.go
+++ bases-go/example-09/data-client/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	readNonExistingFile("")
+	fmt.Println("Execution finished")
+}
+
+// Function that reads a non-existing file and returns execute a panic
+func readNonExistingFile(path string) *os.File {
+	// Defer function to close file and recover panic
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recover from panic:", r)
+		}
+	}()
+
+	file, err := os.Open("non-existing-file.txt")
+	if err != nil {
+		panic("The indicated file was not found or is damaged")
+	}
+	defer file.Close()
+
+	return file
 }
